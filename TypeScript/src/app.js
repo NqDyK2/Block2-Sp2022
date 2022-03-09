9ddc8c91d964a41b48d0b12f1ab822f9ca1f9192
NqDyK2 <88083327+NqDyK2@users.noreply.github.com>
2022-03-09 20:04:45 +0700

    feat:Day 2 with TypeScript

---
+++ TypeScript/src/app.js
@@ -0,0 +1,18 @@
+function sum(sA, sB) {
+    return sA + sB;
+}
+console.log(sum(5, 6));
+var a = 20;
+var Name = "Dyy";
+var myObj = { id: 1, price: 200 };
+var myArr = ["Dyy", "vfgs"];
+var myArrNum = [1, 2, 3, 4, 5, 6];
+var myArrOb = [
+    { id: 1, name: "Dyy", price: 200 },
+    { id: 1, name: "Dyy", status: true }
+];
+if ('status' in myArrOb == true) {
+    console.log("Yes");
+}
+var ShowwMessage = function (props) {
+};
---
+++ TypeScript/src/app.ts
@@ -0,0 +1,35 @@
+function sum (sA:number , sB: number) {
+    return sA + sB;
+}
+console.log(sum(5,6));
+type Product = {
+    id: number ,
+    name: string,
+    price: number
+}
+type Product2 = {
+    id: number ,
+    name: string,
+    status : boolean,
+}
+type MergeType =  Product | Product2
+
+const a:number = 20;
+const Name: string = "Dyy";
+const myObj : { id: number, price: number} = {id :1 , price: 200}
+const myArr : string[] = ["Dyy" , "vfgs"]
+const myArrNum : number[] = [1,2,3,4,5,6]
+const myArrOb: MergeType[] = [
+    {id: 1 , name: "Dyy" , price : 200},
+    {id: 1 , name: "Dyy" , status: true}
+]
+if('status' in myArrOb == true){
+    console.log("Yes");
+    
+}
+type AppProps = {
+    message : string
+}
+const ShowwMessage = (props: AppProps) => {
+
+}
