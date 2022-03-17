1f21499aa2a7f67be5dabd9c2d1f40d56e32f765
NqDyK2 <88083327+NqDyK2@users.noreply.github.com>
2022-03-18 01:14:50 +0700

    feat:Update Day5 with react-type

--- React-Type/src/App.tsx
+++ React-Type/src/App.tsx
@@ -5,9 +5,15 @@ import ShowInfo from './components/ShowInfor'
 
 import type { Product } from './types/product';
 import { list } from './api/product';
-import { NavLink, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/Website/HomePage';
-import ProductPage from './pages/Website/ProductPage';
+import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
+import ProductPage from './pages/ProductPage';
+import ProductDetail from './pages/ProductDetail';
+import AdminLayout from './pages/layouts/AdminLayout';
+import Dashboard from './pages/Dashboard';
+import HomePage from './pages/HomePage';
+import WebsiteLayout from './pages/layouts/WebsiteLayout';
+// import HomePage from './pages/Website/HomePage';
+// import ProductPage from './pages/Website/ProductPage';
 
 function App() {
   const [products, setProducts] = useState<{_id: number, name: string}[]>([])
@@ -30,12 +36,21 @@ function App() {
         </header>
         <main>
           <Routes>
-            <Route path='/' element={<HomePage />}>
-              
+            <Route path="/" element={<WebsiteLayout />}>
+                <Route index element={<HomePage />} />
+                {/* Cách 1: để vào chi tiết sản phẩm 
+                <Route path="product">
+                  <Route element={<ProductPage />} />
+                  <Route path=":id" element={<ProductDetail />} />
+                </Route> */}
+                {/* Cách 2: để vào chi tiết sản phẩm */}
+                  <Route path="product" element={<ProductPage />}  />
+                  <Route path="/product/:id" element={<ProductDetail />} />
             </Route>
-            <Route path="/" element={<HomePage />}/>
-            <Route path="product" element={<ProductPage />}/>
-            <Route path="about" element={<h1>About page</h1>}/>
+            <Route path="admin" element={<AdminLayout />}>
+                <Route index element={<Navigate to="dashboard" />} />
+                <Route path="dashboard" element={<Dashboard />} />
+            </Route>
           </Routes>
         </main>
     </div>
